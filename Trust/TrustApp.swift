b870c35c82be24ef687012bb902efa9167bdc778
Samhita <meetu2005@icloud.com>
2023-06-28 20:55:40 -0400

    Initial Commit

---
+++ Trust/TrustApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrustApp.swift
+//  Trust
+//
+//  Created by Samhita Prabhu on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TrustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
